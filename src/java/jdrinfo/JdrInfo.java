cf4342b89750c0d0f38a73c215f9c130ddf44962
Nicola Talbot <n.talbot@uea.ac.uk>
2025-11-24 19:42:50 +0000

    integrate texjavahelplib into jdrinfo

--- src/java/jdrinfo/JdrInfo.java
+++ src/java/jdrinfo/JdrInfo.java
@@ -5,7 +5,7 @@
 //                 http://www.dickimaw-books.com/
 
 /*
-    Copyright (C) 2006 Nicola L.C. Talbot
+    Copyright (C) 2014 Nicola L.C. Talbot
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -23,10 +23,26 @@
 */
 package com.dickimawbooks.jdrinfo;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import java.util.Locale;
+import java.util.Vector;
+
+import com.dickimawbooks.texjavahelplib.TeXJavaHelpLib;
+import com.dickimawbooks.texjavahelplib.TeXJavaHelpLibAppAdapter;
+import com.dickimawbooks.texjavahelplib.InvalidSyntaxException;
+import com.dickimawbooks.texjavahelplib.CLISyntaxParser;
+import com.dickimawbooks.texjavahelplib.CLIArgValue;
+import com.dickimawbooks.texjavahelplib.HelpSetLocale;
+
 import com.dickimawbooks.jdr.*;
 import com.dickimawbooks.jdr.io.*;
 import com.dickimawbooks.jdr.exceptions.*;
+
+import com.dickimawbooks.jdrresources.JDRResources;
 
 /**
  * Gets information about a JDR or AJR file.
@@ -36,8 +52,241 @@ public class JdrInfo implements FileFilter
 {
    public JdrInfo()
    {
+      inFileNames = new Vector<String>();
    }
 
+   public int getExitCode()
+   {
+      return helpLibApp == null ? exitCode : helpLibApp.getExitCode();
+   }
+
+   public void setExitCode(int code)
+   {
+      if (helpLibApp != null)
+      {
+         helpLibApp.setExitCode(code);
+      }
+
+      exitCode = code;
+   }
+
+   public int getExitCode(Throwable e, boolean isFatal)
+   {
+      if (helpLibApp != null)
+      {
+         return helpLibApp.getExitCode(e, isFatal);
+      }
+      else if (e instanceof InvalidSyntaxException)
+      {
+         return TeXJavaHelpLibAppAdapter.EXIT_SYNTAX;
+      }
+      else if (e instanceof IOException)
+      {
+         return TeXJavaHelpLibAppAdapter.EXIT_IO;
+      }
+      else
+      {
+         return TeXJavaHelpLibAppAdapter.EXIT_OTHER;
+      }
+   }
+
+   protected void initHelpLibrary() throws IOException
+   {
+      helpLibApp = new TeXJavaHelpLibAppAdapter()
+       {
+          @Override
+          public boolean isGUI() { return false; }
+
+          @Override
+          public String getApplicationName()
+          {
+             return NAME;
+          }
+
+          @Override
+          public boolean isDebuggingOn()
+          {
+             return debugMode;
+          }
+
+          @Override
+          public void message(String msg)
+          {
+             if (verboseLevel > 0)
+             {
+                stdOutMessage(msg);
+             }
+          }
+
+       };
+
+      if (helpSetLocale == null)
+      {
+         helpSetLocale = new HelpSetLocale(Locale.getDefault());
+      }
+
+      helpLib = new TeXJavaHelpLib(helpLibApp, NAME,
+       "/resources", "/resources/dictionaries",
+       helpSetLocale, helpSetLocale, "jdrcommon", "jdrinfo");
+
+      helpLibApp.setHelpLib(helpLib);
+
+      if (exitCode != 0)
+      {
+         helpLibApp.setExitCode(exitCode);
+      }
+   }
+
+   public TeXJavaHelpLib getHelpLib()
+   {
+      return helpLib;
+   }
+
+   public String getMessageWithFallback(String label,
+       String fallbackFormat, Object... params)
+   {
+      return helpLib.getMessageWithFallback(label, fallbackFormat, params);
+   }
+
+   public String getMessageIfExists(String label, Object... args)
+   {
+      if (helpLib == null) return null;
+
+      return helpLib.getMessageIfExists(label, args);
+   }
+
+   public String getMessage(String label, Object... params)
+   {
+      if (helpLib == null)
+      {// message system hasn't been initialised
+
+         String param = (params.length == 0 ? "" : params[0].toString());
+
+         for (int i = 1; i < params.length; i++)
+         {
+            param += ","+params[0].toString();
+         }
+
+         return String.format("%s[%s]", label, param);
+      }
+
+      return helpLib.getMessage(label, params);
+   }
+
+   public void error(String message, Throwable e)
+   {
+      if (helpLibApp != null)
+      {
+         helpLibApp.error(message, e);
+      }
+      else
+      {
+         message("error", message, e);
+         setExitCode(getExitCode(e, false));
+      }
+   }
+
+   public void message(String msgTag, String message, Throwable e)
+   {
+      if (message == null)
+      {
+         if (e != null)
+         {
+            message = e.getMessage();
+         }
+
+         if (message == null)
+         {
+            message = e.getClass().getSimpleName();
+         }
+      }
+
+      if (e != null)
+      {
+         System.err.format("%s: %s: %s%n", getApplicationName(), msgTag, message);
+
+         if (debugMode)
+         {
+            e.printStackTrace();
+         }
+      }
+      else if (msgTag.contains("error"))
+      {
+         System.err.format("%s: %s: %s%n", getApplicationName(), msgTag, message);
+      }
+      else
+      {
+         System.out.format("%s: %s: %s%n", getApplicationName(), msgTag, message);
+      }
+   }
+
+   public void syntax()
+   {
+      versionInfo();
+
+      System.out.println();
+      System.out.println(getMessage("clisyntax.usage",
+        getMessage("syntax.options", getApplicationName())));
+
+      System.out.println();
+
+      helpLib.printSyntaxItem(getMessage("syntax.in", "--in", "-i"));
+
+      System.out.println();
+
+      helpLib.printSyntaxItem(getMessage("syntax.debug", "--[no]debug"));
+
+      helpLib.printSyntaxItem(getMessage("clisyntax.version2", "--version", "-v"));
+
+      helpLib.printSyntaxItem(getMessage("clisyntax.help2", "--help", "-h"));
+
+      System.out.println();
+
+      System.out.println(getMessage("clisyntax.bugreport",
+        "https://github.com/nlct/flowframtk"));
+   }
+
+   public void versionInfo()
+   {
+      if (!shownVersion)
+      {
+         System.out.println(getMessageWithFallback("about.version_date",
+           "{0} version {1} ({2})", getApplicationName(),
+           JDRResources.APP_VERSION, JDRResources.APP_DATE));
+         shownVersion = true;
+      }
+   }
+
+   public void license()
+   {
+      System.out.println();
+      System.out.format("Copyright %s Nicola Talbot%n",
+       getCopyrightDate());
+      System.out.println(getMessage("about.license"));
+      System.out.println("https://github.com/nlct/flowframtk");
+   }
+
+   public String getCopyrightStartYear()
+   {
+      return "2025";
+   }
+
+   public String getCopyrightDate()
+   {
+      String startYr = getCopyrightStartYear();
+      String endYr = JDRResources.APP_DATE.substring(0, 4);
+
+      if (startYr.equals(endYr))
+      {
+         return endYr;
+      }
+      else
+      {
+         return String.format("%s-%s", startYr, endYr);
+      }
+   }
+
+   @Override
    public boolean accept(File pathname)
    {
       String name = pathname.getName().toLowerCase();
@@ -45,13 +294,22 @@ public class JdrInfo implements FileFilter
       return name.endsWith(".jdr") || name.endsWith(".ajr");
    }
 
+   public String getApplicationName()
+   {              
+      return NAME; 
+   }           
+            
+   public String getApplicationVersion()
+   {        
+      return JDRResources.APP_VERSION;
+   }
 
-   private void checkFile(File file)
+   private void checkFile(File file) throws IOException
    {
       if (!file.exists())
       {
-         System.err.println("jdrinfo: "+"'"+file.toString()+"' doesn't exist");
-         return;
+         throw new FileNotFoundException(
+           getMessage("error.io.file_not_found", file));
       }
 
       try
@@ -62,25 +320,18 @@ public class JdrInfo implements FileFilter
       }
       catch (InvalidFormatException e)
       {
-         System.err.println("jdrinfo: '"+file.toString()
-            +"' is not a JDR or AJR file");
-      }
-      catch (FileNotFoundException e)
-      {
-         System.err.println("jdrinfo: "+e.getMessage());
-      }
-      catch (IOException e)
-      {
-         System.err.println("jdrinfo: unable to read file '"+file.toString()+"'");
-         System.err.println(e);
+         System.err.format("jdrinfo: %s%n",
+           getMessage("error.not_jdr_or_ajr", file));
+
+         setExitCode(TeXJavaHelpLibAppAdapter.EXIT_RUNTIME);
       }
    }
 
-   public void checkFiles(String[] args)
+   protected void run() throws IOException
    {
-      for (int i = 0; i < args.length; i++)
+      for (int i = 0; i < inFileNames.size(); i++)
       {
-         File file = new File(args[i]);
+         File file = new File(inFileNames.get(i));
 
          if (file.isDirectory())
          {
@@ -109,28 +360,157 @@ public class JdrInfo implements FileFilter
       System.err.println("There is NO WARRANTY. See accompanying licence file for details.");
    }
 
-   /**
-    * Prints syntax information to STDERR and quits.
-    */
-   public static void syntax()
+   private void parseArgs(String[] args) throws InvalidSyntaxException
    {
-      System.err.println("Syntax: jdrinfo <file or directory>+");
-      System.exit(0);
+      CLISyntaxParser cliParser = new CLISyntaxParser(helpLib, args, "-h", "-v")
+      {
+         @Override
+         protected int argCount(String arg)
+         {
+            if (arg.equals("--in") || arg.equals("-i"))
+            {
+               return 1;
+            }
+
+            return 0;
+         }
+
+         @Override
+         public boolean setDebugOption(String option, Integer value)
+         throws InvalidSyntaxException
+         {
+            debugMode = true;
+          
+            return true;
+         }
+      
+         @Override
+         protected boolean preparseCheckArg()
+         throws InvalidSyntaxException
+         {
+            if (super.preparseCheckArg())
+            {
+               return true;
+            }
+          
+            if (originalArgList[preparseIndex].equals("--verbose"))
+            {
+               verboseLevel = 1;
+            }
+            else if (originalArgList[preparseIndex].equals("--noverbose"))
+            {
+               verboseLevel = 0;
+            }
+            else if (originalArgList[preparseIndex].equals("--nodebug")
+                   || originalArgList[preparseIndex].equals("--no-debug")
+                    )
+            {
+               debugMode = false;
+            }
+            else
+            {
+               return false;
+            }
+
+            return true;
+         }
+
+         @Override
+         protected void help()
+         {
+            syntax();
+            System.exit(0);
+         }
+
+         @Override
+         protected void version()
+         {
+            System.out.println(helpLib.getAboutInfo(false,
+             JDRResources.APP_VERSION, JDRResources.APP_DATE,
+             String.format(
+              "Copyright (C) %s Nicola L. C. Talbot (%s)",
+              getCopyrightDate(),
+              JDRResources.COPYRIGHT_YEAR,
+              helpLib.getInfoUrl(false, "www.dickimaw-books.com")),
+              TeXJavaHelpLib.LICENSE_GPL3,
+              false, null
+            ));
+
+            System.exit(0);
+         }
+
+         @Override
+         protected void parseArg(String arg)
+         throws InvalidSyntaxException
+         {
+            inFileNames.add(arg);
+         }
+
+         @Override
+         protected boolean parseArg(String arg, CLIArgValue[] returnVals)
+         throws InvalidSyntaxException
+         {
+            if (isArg(arg, "--in", "-i", returnVals))
+            {
+               if (returnVals[0] == null)
+               {
+                  throw new InvalidSyntaxException(
+                     getMessage("error.clisyntax.missing.value", arg));
+               }
+
+               inFileNames.add(returnVals[0].toString());
+            }
+            else
+            {
+               return false;
+            }
+
+            return true;
+         }
+      };
+
+      cliParser.process();
+
+      if (inFileNames.isEmpty())
+      {
+         throw new InvalidSyntaxException(
+            getMessage("error.syntax.missing_in"));
+      }
    }
 
    public static void main(String[] args)
    {
-      if (args.length == 0)
-      {
-         System.err.println("Missing input file(s)");
-         System.err.println("Use '-help' for help");
+      final JdrInfo app = new JdrInfo();
 
-         System.exit(1);
+      try
+      {
+         app.initHelpLibrary();
+         app.parseArgs(args);
+         app.run();
+      }
+      catch (InvalidSyntaxException e)
+      {
+         app.error(e.getMessage(), null);
+         app.setExitCode(TeXJavaHelpLibAppAdapter.EXIT_SYNTAX);
+      }
+      catch (Throwable e)
+      {
+         app.error(null, e);
       }
 
-      JdrInfo info = new JdrInfo();
-
-      info.checkFiles(args);
+      System.exit(app.getExitCode());
    }
 
+   protected int exitCode = 0;
+   protected boolean shownVersion = false;
+   protected boolean debugMode = false;
+   protected int verboseLevel = 0;
+
+   private Vector<String> inFileNames;
+
+   private TeXJavaHelpLib helpLib;
+   private TeXJavaHelpLibAppAdapter helpLibApp;
+   private HelpSetLocale helpSetLocale;
+
+   public static final String NAME = "jdrinfo";
 }
