0a6c6cbc39e08aa11c07b97d20278afebd56d521
Nicola Talbot <n.talbot@uea.ac.uk>
2026-04-25 13:34:02 +0100

    updated version to 0.9.1

--- src/java/jdrresources/JDRResources.java
+++ src/java/jdrresources/JDRResources.java
@@ -1888,7 +1888,7 @@ public class JDRResources
         START_COPYRIGHT_YEAR, COPYRIGHT_YEAR,
         helpLib.getInfoUrl(html, "www.dickimaw-books.com")),
         TeXJavaHelpLib.LICENSE_GPL3,
-        false, null
+        true, null
       );
    }
 
@@ -3423,8 +3423,8 @@ public class JDRResources
    public static final int LABEL_SPACER=5;
    public static final int BUTTON_SPACER=10;
 
-   public static final String APP_VERSION = "0.9";
-   public static final String APP_DATE = "2026-04-01";
+   public static final String APP_VERSION = "0.9.1";
+   public static final String APP_DATE = "2026-04-25";
    public static final String START_COPYRIGHT_YEAR = "2005";
    public static final String COPYRIGHT_YEAR
     = APP_DATE.substring(0,4);
